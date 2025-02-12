acfbdbd5646a4278de2c975fa2a1ee52cb2f2d37
Bartosz Mogilan <bmogilan@gmail.com>
2025-02-12 16:21:29 +0000

    updates

--- server/routes/passport-setup.js
+++ server/routes/passport-setup.js
@@ -1,6 +1,9 @@
 const passport = require('passport');
 const googleStrategy = require('passport-google-oauth20').Strategy;
 const User = require('./User');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 passport.serializeUser((user, done) => {
     done(null, user.id);
